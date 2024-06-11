495eaa1847c95c87a3aec249418d29012c8351da
Henry <5065089+nguyengg@users.noreply.github.com>
2024-06-11 08:47:44 -0700

    bug: fix file handles still opened until extract program exits

--- internal/extract/command.go
+++ internal/extract/command.go
@@ -69,68 +69,24 @@ func (c *Command) Execute(args []string) error {
 }
 
 // extract extracts the content of the named ZIP file and returns the newly created directory.
-func (c *Command) extract(ctx context.Context, name string) (output string, err error) {
+func (c *Command) extract(ctx context.Context, name string) (string, error) {
 	r, err := zip.OpenReader(name)
 	if err != nil {
 		return "", err
 	}
 	defer r.Close()
 
-	// go through all the files first to see if they're all under the same root directory. if not, create a new one.
-	// this is similar to what Mac does when uncompressing files.
-	root := ""
-findRootLoop:
-	for _, f := range r.File {
-		if f.FileInfo().IsDir() {
-			continue
-		}
-
-		switch paths := strings.SplitN(f.Name, "/", 2); {
-		case len(paths) == 1:
-			// no root dir so must create one.
-			log.Printf("%s has no dir", f.Name)
-			fallthrough
-		case root != "" && root != paths[0]:
-			// multiple roots, must create a common one.
-			log.Printf("%s has different root from %s", f.Name, root)
-			root = ""
-			break findRootLoop
-		default:
-			root = paths[0]
-		}
-
-		select {
-		case <-ctx.Done():
-			return "", ctx.Err()
-		default:
-		}
+	topLevelDir, err := c.topLevelDir(ctx, r.File)
+	if err != nil {
+		return "", err
 	}
 
-	// even if common root exists for files in the archive, a directory with the same name may exist on fs.
-	// in that case, we'll use the archive's name to create a unique dir.
 	stem, _ := internal.SplitStemAndExt(name)
-	output = root
-	if output == "" {
-		output = stem
-		root = stem
-	} else {
-		root = ""
-	}
-createOutputLoop:
-	for i := 0; ; {
-		switch err = os.Mkdir(output, 0755); {
-		case err == nil:
-			break createOutputLoop
-		case errors.Is(err, os.ErrExist):
-			i++
-			output = stem + "-" + strconv.Itoa(i)
-			root = output
-		default:
-			return "", fmt.Errorf("create directory error: %w", err)
-		}
+	output, pathFn, err := c.createOutputDir(topLevelDir, stem)
+	if err != nil {
+		return "", err
 	}
 
-	// now decompress with progress report.
 	sometimes := rate.Sometimes{Interval: 5 * time.Second}
 	n := len(r.File)
 	for i, f := range r.File {
@@ -138,19 +94,7 @@ createOutputLoop:
 			continue
 		}
 
-		path := filepath.Join(root, f.Name)
-		if err = os.MkdirAll(filepath.Dir(path), f.Mode()); err != nil {
-			_ = os.Remove(output)
-			return "", err
-		}
-
-		w, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-		if err != nil {
-			_ = os.Remove(output)
-			return "", err
-		}
-
-		if err = c.copy(ctx, i+1, n, w, f); err != nil {
+		if err = c.copy(ctx, i+1, n, pathFn(f.Name), f); err != nil {
 			_ = os.Remove(output)
 			return "", err
 		}
@@ -169,10 +113,81 @@ createOutputLoop:
 	return output, nil
 }
 
+// topLevelDir returns the top-level directory that is ancestor to all files in the archive.
+//
+// This exists only if all files in the archive has the same top-level directory. If at least two files don't share the
+// same top-level directory, return an empty string. If the archive contains only one file but the file does not belong
+// to any directory, an empty string is also returned.
+func (c *Command) topLevelDir(ctx context.Context, files []*zip.File) (root string, err error) {
+	for _, f := range files {
+		if f.FileInfo().IsDir() {
+			continue
+		}
+
+		switch paths := strings.SplitN(f.Name, "/", 2); {
+		case len(paths) == 1:
+			// no root dir so must create one.
+			fallthrough
+		case root != "" && root != paths[0]:
+			root = ""
+			return
+		default:
+			root = paths[0]
+		}
+
+		select {
+		case <-ctx.Done():
+			return "", ctx.Err()
+		default:
+		}
+	}
+
+	return
+}
+
+// createOutputDir creates the output directory and the function to join the output path for each file in the archive.
+func (c *Command) createOutputDir(topLevelDir, stem string) (output string, pathFn func(string) string, err error) {
+	if output = topLevelDir; output != "" {
+		pathFn = func(s string) string {
+			return filepath.Join("", s)
+		}
+	} else {
+		output = stem
+		pathFn = func(s string) string {
+			return filepath.Join(stem, s)
+		}
+	}
+
+	for i := 0; ; {
+		switch err = os.Mkdir(output, 0755); {
+		case err == nil:
+			return
+		case errors.Is(err, os.ErrExist):
+			i++
+			output = stem + "-" + strconv.Itoa(i)
+			pathFn = func(s string) string {
+				return filepath.Join(output, s)
+			}
+		default:
+			return "", nil, err
+		}
+	}
+}
+
 // copy is an implementation of io.Copy that is cancellable and also provides progress report.
-func (c *Command) copy(ctx context.Context, i, n int, w io.Writer, f *zip.File) (err error) {
+func (c *Command) copy(ctx context.Context, i, n int, path string, f *zip.File) error {
 	sometimes := rate.Sometimes{Interval: 5 * time.Second}
 	sometimes.Do(func() {})
+
+	if err := os.MkdirAll(filepath.Dir(path), f.Mode()); err != nil {
+		return err
+	}
+
+	w, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+	if err != nil {
+		return err
+	}
+	defer w.Close()
 
 	r, err := f.Open()
 	if err != nil {
